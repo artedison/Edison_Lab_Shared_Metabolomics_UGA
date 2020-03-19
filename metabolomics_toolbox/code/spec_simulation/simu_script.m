40c2fc7f8f0140e8eb06ab0504c97a5e6d532563
mikeaalv <yuewu_mike@163.com>
2020-03-19 09:39:16 -0400

    merge the spectral simulaiton code with updated on on private toolbox

--- metabolomics_toolbox/code/spec_simulation/simu_script.m
+++ metabolomics_toolbox/code/spec_simulation/simu_script.m
@@ -6,36 +6,39 @@ close all;
 clear all;
 comp='/Users/yuewu/';%the computer user location
 workdir=[comp 'Dropbox (Edison_Lab@UGA)/Projects/Bioinformatics_modeling/spectral.related/spec_simu_gissmo/'];%%the working folder
-libdir=[comp 'Dropbox (Edison_Lab@UGA)/Resources/gissmo_lib/'];
+libdir=[comp 'Dropbox (Edison_Lab@UGA)/Resources/gissmo_lib/cleandata/'];
 cd(workdir);
 rng(1);%%random seed
 args=struct();%% all the arguments
 
 %%%%%%%%%%%%%%USER ARGUMENT%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%sample control
+args.nsample=100;%%total number of samples
+ngroup=2;
+subsampnum=[50 50];%%please make sure sum(subsampnum)=args.nsample
+args.sampleindvec=[repmat(1,1,subsampnum(1)) repmat(2,1,subsampnum(2))];%%use this to control samples of each group
 %%concentration control
 args.conc_range=[0 1000];%%the whole dynamic range of all compounds
 args.totalnoiseregion=[-0.5 -0.2];%% the region of no signal to shift spectra
-args.factor_std=0.3;%%the relative variance for compound concentration in samples
+args.factor_std=0.3*[repmat(1,1,subsampnum(1)) 1.5*repmat(2,1,subsampnum(2))];%0.3;%%the relative variance for compound concentration in samples can also be a vector for indicate all the compound specifically
 args.sigma_noise=15;%white noise on the whole spectra
-%%sample control
-args.nsample=50;%%number of samples for each group
-args.sampleindvec=[repmat(1,1,args.nsample) repmat(2,1,args.nsample)];%%use this to control samples
 %%type of simulation: 'random_chemical_sample' OR 'biological_sample'
-args.simutype='biological_sample';
+args.simutype='biological_sample' %'biological_sample';
 args.group_vec={'Light' 'Dark'}; %different biological groups
 %%phenotype control
 args.pheno_vec={'growth_rate' 'weight' 'offsprings'};
 args.phenorang=[0 10];% the range of phenotype value
 %% compound control
 args.compd_vec={'ATP' 'Acetic-acid' 'Adenosine' 'Betaine' 'Choline' 'Citrate' 'Cytosine' 'DSS' 'Ethanol' 'Formate' 'Fumaric-acid' 'Glycerol' 'Glycine' 'L-Serine' 'L-Threonine' 'L-Valine' 'L-alanine' 'L-arginine' 'L-glutamic-acid' 'L-malic-acid'};
-%%concentration that are changed between groups
+%%concentration that are changed between groups (can have no this field)
 %%%{compound, relative mean concentration in different samples}
 args.modif_conc_list=cell2table({'L-arginine' 1 3; 'Choline' 2 1});
-%%correlation between compound and phenotypes
+%%correlation between compound and phenotypes (can have no this field)
 %%%group index, compound, phenotype index, simulated correlation
 args.corr_conc_list=cell2table({2 'Glycine' 1 0.8});
 %%technical control
 args.lambda=-0.00035;% the exponential window function for fid, need to be modified according to inspection of result spectra
+args.exitprob=1;% only used in 'random_chemical_sample' the compound can be totally missing from the spectra if 1 (by p=0.5). the listed compounds must be in the spetra if 0
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%%sample group name vector
@@ -78,9 +81,18 @@ compd_vec=args.compd_vec;
 pheno_tab=array2table(pheno_mat,'VariableNames',args.pheno_vec);
 save('simu_spect.mat','conc_mat','spec_mat','ppm','pheno_mat','pheno_tab','group_ident_vec','compd_vec','pheno_vec','group_vec','sampleindvec');
 
+%%checks
+% surf(conc_mat);
+% heatmap(corrcoef(conc_mat));
+% corrcoef(conc_mat(:,5),conc_mat(:,18))
+% mean(conc_mat(51:100,5))/mean(conc_mat(1:50,5))%0.5
+% mean(conc_mat(51:100,18))/mean(conc_mat(1:50,18))%3.0
+% corrcoef(conc_mat(51:100,13),pheno_mat(51:100,1))%0.8
+% plot(conc_mat(51:100,13),pheno_mat(51:100,1));
+
 % X=spec_mat;
 % Yvec=args.sampleindvec;
-% save('simu_spect_student.mat','X','ppm','Yvec');%%spec_mat: X sampleindvec: Yvec
-%%plot test
+% % save('simu_spect_student.mat','X','ppm','Yvec');%%spec_mat: X sampleindvec: Yvec
+% %plot test
 % plotr(ppm,spec_mat(2,:));
 % stackSpectra(spec_mat,ppm,0.0,10,'simulated spectra');
--- metabolomics_toolbox/code/spec_simulation/spec_conc_simu.m
+++ metabolomics_toolbox/code/spec_simulation/spec_conc_simu.m
@@ -1,10 +1,11 @@
 function [simures] = spec_conc_simu(args)
 %% the function to generate concentration matrix (sample X compound)
+%% find out the detailed algorithm description here /Dropbox (Edison_Lab@UGA)/Projects/Bioinformatics_modeling/spectral.related/spec_simu_gissmo/spectra.simu.method.key
 % arguments: args
-%     args.conc_range: the whole dynamic range of all compounds. default [0 1000]
-%     args.nsample: number of samples. default 50
+%     args.conc_range: the general whole dynamic range of all compounds. default [0 1000]
+%     args.nsample: number of samples. default 100
 %     args.simutype: type of simulation: 'random_chemical_sample' OR 'biological_sample'. One of the type must be provided.
-%%%         'random_chemical_sample': each sample is considerred independent and compound concentrations are random distributed. For each compound, a random range was chosen and the range is used to transform sample from normal distribution to produce concentrations for each time point.
+%%%         'random_chemical_sample': each sample is considerred independent and compound concentrations are random distributed. For each compound, a random mean_concentration was chosen and the mean_concentration is used to transform sample from normal distribution to produce concentrations for each sample.
 %%%         'biological_sample': N biological samples with some compound concentrations different from the samples
 %     args.factor_std: the relative variance for compound concentration in samples. default 0.3
 %     args.compd_vec: the simulated compound list. Must be provided. {'A' 'B' 'C'}
@@ -14,6 +15,7 @@ function [simures] = spec_conc_simu(args)
 %         EX:  cell2table({2 'Glycine' 1 0.8});
 %     args.sampleindvec: sample index vector. EX. [1 1 1 2 2 2] two groups and each with 3 samples
 %     args.pheno_mat: the phenotype matrix (sample X phenotypes). Should be provided if corr_conc_list is not empty
+%     args.exitprob: (only used in random_chemical_sample). the compound can be totally missing from the spectra if 1 (by p=0.5). the listed compounds must be in the spetra if 0. default 0
 %
 % Return: simures:
 %               conc_mat: the concentration matrix (sample X compound)
@@ -23,7 +25,7 @@ if ~isfield(args,'conc_range')
   args.conc_range=[0 1000];
 end
 if ~isfield(args,'nsample')
-  args.nsample=50;
+  args.nsample=100;
 end
 if ~isfield(args,'simutype')
   error('please provide simulation types');
@@ -35,16 +37,22 @@ if ~isfield(args,'compd_vec')
   error('please provide compound list');
 end
 if ~isfield(args,'modif_conc_list')
-  args.modif_conc_list=cell2table({nan nan nan;});
+  args.modif_conc_list=cell2table({nan nan nan;});%%
 end
-if ~isfield(args,'pheno_mat') && ~isfield(args,'corr_conc_list')
+if ~isfield(args,'pheno_mat') && isfield(args,'corr_conc_list')
   error('please provide phenotype matrix');
+end
+if ~isfield(args,'pheno_mat')
+  args.pheno_mat=nan;
 end
 if ~isfield(args,'corr_conc_list')
   args.corr_conc_list=cell2table({nan nan nan nan;});
 end
 if ~isfield(args,'sampleindvec')
   error('please provide sample vector');
+end
+if ~isfield(args,'exitprob')
+  args.exitprob=0;
 end
 
 conc_range=args.conc_range;
@@ -57,18 +65,26 @@ corr_conc_list=args.corr_conc_list;
 sampleindvec=args.sampleindvec;
 pheno_mat=args.pheno_mat;
 
-factor_std_vec=[repmat(factor_std,1,10) repmat(factor_std*1.5,1,10)];
+if length(factor_std)>1
+  factor_std_vec=factor_std;
+elseif length(factor_std)==1
+  factor_std_vec=repmat(factor_std,1,length(compd_vec));
+end
 %%% simulating different compound concentration for different samples
 conc_mat=zeros(length(sampleindvec),length(compd_vec));
 for compdi=1:length(compd_vec)
   compd=compd_vec{compdi};
   %%random generate range for the compound between a preset range
-  crang=sort(rand(1,2)*(conc_range(2)-conc_range(1))+conc_range(1));%%defined the range for a compound (default)
+  meanconc=rand(1)*(conc_range(2)-conc_range(1))+conc_range(1);
+  % crang=sort(rand(1,2)*(conc_range(2)-conc_range(1))+conc_range(1));%%defined the range for a compound (default)
   if strcmp(simutype,'random_chemical_sample')
     for samplei=1:length(sampleindvec)
       %%randomness in existence * randome sampled concentration
-      cexist=randi(2)-1;
-      cconc=exp(normrnd(0,factor_std_vec(compdi)))*(crang(2)-crang(1))+crang(1);%%log normal distributed for a small factor_std it is close to linear
+      cexist=1;
+      if args.exitprob==1
+        cexist=randi(2)-1;
+      end
+      cconc=exp(normrnd(0,factor_std_vec(compdi)))*meanconc;%%log normal distributed for a small factor_std it is close to linear
       conc_mat(samplei,compdi)=cexist*cconc;
     end
   elseif strcmp(simutype,'biological_sample')
@@ -93,7 +109,7 @@ for compdi=1:length(compd_vec)
       ratio=repmat(1,1,length(sampleindvec));
     end
     %%exp to make sure positive
-    conc_mat(:,compdi)=(exp(normrnd(0,factor_std_vec(compdi),length(sampleindvec),1))*(crang(2)-crang(1))+crang(1)).*ratio';
+    conc_mat(:,compdi)=(exp(normrnd(0,factor_std_vec(compdi),length(sampleindvec),1))*meanconc).*ratio';
     %%correlations between compound and phenotypes
     if length(corr_compd_ind)~=0
       corr_conc_vec=table2cell(corr_conc_list(corr_compd_ind,:));
