aed8499b32ad0a6c88c4498bd823e6e896b65bf9
Laura C. Morris <lmorris@ccrc.uga.edu>
2022-02-10 22:13:34 -0500

    Mathworks File Exchange Licenses
    Mathworks File Exchange License and current Cite As added to functions from other authors.

--- metabolomics_toolbox/code/pipe2matlab.m
+++ metabolomics_toolbox/code/pipe2matlab.m
@@ -4,8 +4,45 @@ function spectra=pipe2matlab(filepath)
 % Greg Stupp UF
 % Reads in an nmrPipe ft file. Outputs
 % spectra.real,spectra.ppm1,spectra.ppm2,spectra.Title
-% Uses functions borrowed from Covariance NMR Toolbox, ver. 1.0b, (C) (2010) David A. Snyder(1) along with Timothy Short(1),
-%   Leigh Alzapiedi(1) and Rafael Br�schweiler (2)
+% Uses functions borrowed from:
+%
+% Covariance NMR Toolbox, ver. 1.0b, (C) (2010) David A. Snyder(1) along with Timothy Short(1),
+%     Leigh Alzapiedi(1) and Rafael Br�schweiler (2)
+%     (1) Department of Chemistry, College of Science and Health, William Paterson University
+%     (2) Department of Chemistry and NHMFL, Florida State University
+%
+% License
+%
+% Copyright (c) 2010, David Snyder
+% All rights reserved.
+%
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are met:
+%
+% * Redistributions of source code must retain the above copyright notice, this
+%  list of conditions and the following disclaimer.
+%
+% * Redistributions in binary form must reproduce the above copyright notice,
+%   this list of conditions and the following disclaimer in the documentation
+%   and/or other materials provided with the distribution
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+% DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+% FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+% DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+% SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+% CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+% OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+% OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+%
+% Currently, Cite As
+% David Snyder (2022). Covariance NMR Toolbox
+% (https://www.mathworks.com/matlabcentral/fileexchange/27264-covariance-nmr-toolbox)
+% MATLAB Central File Exchange. Retrieved February 11, 2022.
+
+
+
 %
 % Arguments:
 % name                 name of file (including extension)
@@ -23,5 +60,3 @@ end
 spectra.real=spectra.real';
 spectra.Title=title;
 end
-
-
--- metabolomics_toolbox/packages/SECIM_GUI/loadFileFunctions/get_header_data.m
+++ metabolomics_toolbox/packages/SECIM_GUI/loadFileFunctions/get_header_data.m
@@ -7,6 +7,36 @@ function header_data = get_header_data(header_in)
 %     Leigh Alzapiedi(1) and Rafael Br�schweiler (2)
 %     (1) Department of Chemistry, College of Science and Health, William Paterson University
 %     (2) Department of Chemistry and NHMFL, Florida State University
+%
+% License
+%
+% Copyright (c) 2010, David Snyder
+% All rights reserved.
+%
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are met:
+%
+% * Redistributions of source code must retain the above copyright notice, this
+%  list of conditions and the following disclaimer.
+%
+% * Redistributions in binary form must reproduce the above copyright notice,
+%   this list of conditions and the following disclaimer in the documentation
+%   and/or other materials provided with the distribution
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+% DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+% FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+% DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+% SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+% CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+% OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+% OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+%
+% Currently, Cite As
+% David Snyder (2022). Covariance NMR Toolbox
+% (https://www.mathworks.com/matlabcentral/fileexchange/27264-covariance-nmr-toolbox)
+% MATLAB Central File Exchange. Retrieved February 11, 2022.
 
 UNUSED_VALUE = -666; % FD's alt. code for zero: there seems to be no SIZE for y-dim nor LB for z and a-dims
 
