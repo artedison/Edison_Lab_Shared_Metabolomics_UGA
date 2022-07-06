5a76178bca680bc507a8a196b59c1752046f4d56
sczhang2 <sz91614@uga.edu>
2022-07-06 10:00:11 -0400

    create_pseudo_peak Annotation change

--- Biosorter-Analysis/Biosorter_Integration/create_pseudo_peak.m
+++ Biosorter-Analysis/Biosorter_Integration/create_pseudo_peak.m
@@ -1,12 +1,12 @@
 function [X] = create_pseudo_peak(mean, sigma, intensities)
 
-% [X] = create_gaussian_curve_Activity(x,0, 1,intensities)
+% [X] = create_pseudo_peak(0, 1,intensities)
 % Description:
 %       Create gaussian curve for each input intensity
 % Input:
-%       mean:         Mean for normal distribution, affect peak center
-%       sigma:        Sigma for normal distribution, affect peak width
-%       intensities:  Intensities related to any given activity 
+%       mean:         1-by-1 double, mean for normal distribution, affect peak center
+%       sigma:        1-by-1 double, sigma for normal distribution, affect peak width
+%       intensities:  1-by-1 double, intensities related to any given activity 
 x=-100:100;
     X = 1/sqrt(2*pi)/sigma*exp(-(x-mean).^2/2/sigma/sigma);
    X = X * intensities; 
